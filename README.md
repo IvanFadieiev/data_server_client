9519de62f02909f7802775a304677de69e8c93dc
IvanFadeev <ivanfadeev91@gmail.com>
2018-01-19 21:26:48 +0200

    Add some stuff

--- README.md
+++ README.md
@@ -4,15 +4,20 @@
 
 ```foreman start -f Procfile.prod``` - to start daemonized scripts
 
-```netcat -z -v localhost 8080 < original_file``` or ```telnet localhost 8080 < original_file```
-
+```netcat -N localhost 1111 < original_file``` or ```telnet localhost 8080 < original_file```
 
 ```
 stream {
    server {
-       listen 1111;
-       proxy_pass localhost:8080;
+       listen localhost:1111;
+       proxy_pass localhost:8079; # proxy host:port
    }
 }
 ```
 add to the very top ```/etc/nginx/nginx.conf```
+
+
+
+data.unpack("B*")[0][0*8...3*8].scan(/.{1,8}/).map { |part|  part.to_i(2).chr }.join
+time = DateTime.parse('1997-01-01').to_time.to_i
+Time.at(time).gmtime
--- lib/servers/receive_initial_data.rb
+++ lib/servers/receive_initial_data.rb
@@ -1,11 +1,18 @@
+require 'byebug'
 # ReseiveInitialData
-module ReseiveInitialData
-  # def post_init
-  # puts "-- someone connected to the echo server!"
-  # end
+class ReseiveInitialData < EventMachine::Connection
+  def post_init
+    puts 'connected' unless ENV['APP_ENV'].eql?('production')
+    set_sock_opt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true)
+    set_sock_opt(Socket::SOL_TCP, Socket::TCP_KEEPIDLE, 10) # 50
+    set_sock_opt(Socket::SOL_TCP, Socket::TCP_KEEPINTVL, 10)
+    set_sock_opt(Socket::SOL_TCP, Socket::TCP_KEEPCNT, 5)
+  end
 
-  def receive_data data
+  def receive_data(data)
+    puts 'disconnected' unless ENV['APP_ENV'].eql?('production')
     # send_data ">>>you sent: #{data}"
+    byebug
     if data.eql?('quit')
       EventMachine.stop_event_loop
     else
@@ -17,3 +24,6 @@ module ReseiveInitialData
   # puts "-- someone disconnected from the echo server!"
   # end
 end
+
+
+# data.unpack("B*")[0][0...3*8].scan(/.{1,8}/).map { |part|  part.to_i(2).chr }.join
